d50ceb7366a52dfd76be7aebbeb7dbc9d98ec491
rachi2006 <rachirachith8@gmail.com>
2025-08-12 08:51:21 +0530

    Create binary search.cpp

---
+++ binary search.cpp
@@ -0,0 +1,30 @@
+//this is the example of binary search sorting
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={10, 20, 30, 40, 50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int low =0;
+    int high=n-1;
+    int val = 50;
+    int index = -1;
+    while(low <= high ){
+        int mid =(low+high)/2;
+        if(arr[mid]==val){
+            index=mid;
+            break;
+        }
+        else if(arr[mid]< val){
+            low= mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    if(index == -1){
+        cout<<"not found"<<endl;
+    }
+    else{
+        cout<<"fount at index:\t"<<index;
+    }
+}
